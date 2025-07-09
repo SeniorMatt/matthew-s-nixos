7070852c2d021336fc015a22fc72b7bf1615e6aa
Senior Matthew <matthew.prakhov@gmail.com>
2025-07-09 16:15:07 +0500

    0.5.6
    - Returned power profiles daemon

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -167,29 +167,7 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "balanced";
-
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 100;
-
-      INTEL_GPU_MIN_FREQ_ON_AC = "500";
-      INTEL_GPU_MIN_FREQ_ON_BAT = "500";
-
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
-    };
-  };
-
+  services.power-profiles-daemon.enable = true;
   services.gnome.gnome-keyring.enable = true;
   services.udisks2.enable = true;
 

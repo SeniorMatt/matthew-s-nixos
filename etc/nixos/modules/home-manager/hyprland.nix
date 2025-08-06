aea4d0adda93b081964e8dba2e246744e0993d7f
Senior Matthew <matthew.prakhov@gmail.com>
2025-08-06 22:36:03 +0500

    0.7.0 - Added hyprland module - Added catppuccin theme module

---
+++ etc/nixos/modules/home-manager/hyprland.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./theme-catppuccin.nix
+  ];
+
+  home.packages = with pkgs; [
+    # System applications
+    wlsunset # Blue light filter
+    tofi # App launcher
+    waybar # Panel
+    hyprpaper # Wallpaper
+    hyprpolkitagent # Authentification agent
+    xdg-desktop-portal-hyprland # Desktop portal hyprland
+    swayosd # Notifications for the volume and brightness
+
+    # Gnome ecosystem
+    evince # Document viewer
+    nautilus # File manager
+    eog # Image viewer
+    papers # Document viewer
+    parlatype # Audio player
+    gnome-calculator # Calculator
+    gnome-clocks # Clock
+
+    # Needs
+    blueberry # Bluetooth control
+    networkmanagerapplet # Network control
+    pavucontrol # Audio control
+    grim # Screenshot
+    slurp # Selector for screenshot
+  ];
+}

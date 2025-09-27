0f0b9367a237d2fd32284ee7e30384b3773db91c
Senior Matthew <matthew.prakhov@gmail.com>
2025-09-27 23:49:06 +0500

    1.3.1
    - Removed Waybar's background for some reason ;D

--- etc/nixos/modules/home-manager/waybar.nix
+++ etc/nixos/modules/home-manager/waybar.nix
@@ -258,7 +258,7 @@
       }
 
       window#waybar {
-          background-color: rgba(30, 30, 46, 1.0);
+          background-color: rgba(30, 30, 46, 0.0);
           border-radius: 0px;
       }
 

57180c25009f7c5311a6bdffa29797ce176d930c
Senior Matthew <matthew.prakhov@gmail.com>
2025-10-26 17:28:40 +0500

    1.8.8
    - Added xwayland support for cosmic.nix
    - Updated Hyprland's animations.nix

--- etc/nixos/hosts/t480/cosmic.nix
+++ etc/nixos/hosts/t480/cosmic.nix
@@ -7,6 +7,7 @@
   ];
 
   services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic.xwayland.enable = true;
 
   home-manager = {
     sharedModules = [
--- etc/nixos/modules/home-manager/hyprland/animations.nix
+++ etc/nixos/modules/home-manager/hyprland/animations.nix
@@ -2,12 +2,12 @@
   wayland.windowManager.hyprland.settings = {
     animations = {
       enabled = "yes";
-      bezier = ["myBezier, 0.05, 0.9, 0.1, 1.1"];
+      bezier = ["myBezier, 0.05, 0.9, 0.1, 1.05"];
       animation = [
-        "windows, 1, 4, myBezier"
-        "windowsOut, 1, 1.5, myBezier, popin 80%"
+        "windows, 1, 3, myBezier"
+        "windowsOut, 1, 4, myBezier, popin 80%"
         "fade, 1, 2, myBezier"
-        "workspaces, 1, 2, default"
+        "workspaces, 1, 2, myBezier, fade"
       ];
     };
   };

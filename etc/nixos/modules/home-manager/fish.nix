4030e21592ab7b9d24ce0ba70809b77c0cc81370
Senior Matthew <matthew.prakhov@gmail.com>
2025-10-27 23:15:04 +0500

    2.0.0
    - Removed oh-my-posh (no need for dat)
    - Removed depricated alias

--- etc/nixos/modules/home-manager/fish.nix
+++ etc/nixos/modules/home-manager/fish.nix
@@ -2,7 +2,7 @@
   programs.fish = {
     enable = true;
     shellInit = ''
-      oh-my-posh init fish | source
+      # oh-my-posh init fish | source
       zoxide init fish | source
       set -g fish_greeting
       alias n="nvim"
@@ -12,15 +12,13 @@
       alias c="ffmpeg -i video.mp4 -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p output.mov"
       alias cr='ffmpeg -i video.mov -vf "scale=1920:-2:force_original_aspect_ratio=decrease:flags=lanczos,fps=60" -c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p -profile:v high -level 4.1 -movflags +faststart -c:a aac -b:a 160k output-reddit.mp4'
       alias bitit="steam-run ~/Documents/builds/latest-build/Bit\ It.x86_64"
-      alias godot="steam-run ~/Documents/Godot_v4.5-stable_mono_linux_x86_64/Godot_v4.5-stable_mono_linux.x86_64"
     '';
   };
 
   programs.oh-my-posh = {
-    enable = true;
+    enable = false;
     enableFishIntegration = true;
     useTheme = "montys";
     # montys - my beloved one
-    # multiverse-neon
   };
 }

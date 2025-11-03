616f6761c70b85d43fa5789b94ccb8d54dcec6c5
Senior Matthew <matthew.prakhov@gmail.com>
2025-11-03 17:03:54 +0500

    2.2.7
    - Now gestures can move you to an unexisted workspace

--- etc/nixos/modules/home-manager/hyprland/gestures.nix
+++ etc/nixos/modules/home-manager/hyprland/gestures.nix
@@ -4,8 +4,8 @@
       # To have 1:1 gestures
       # "3, horizontal, scale: 0.5, workspace"
       # "3, vertical, scale: 0.5, special, magic"
-      "3, left, scale: 3.0, dispatcher, workspace, e+1"
-      "3, right, scale: 4.0, dispatcher, workspace, e-1"
+      "3, left, scale: 3.0, dispatcher, workspace, +1"
+      "3, right, scale: 4.0, dispatcher, workspace, -1"
       "3, vertical, dispatcher, togglespecialworkspace, magic"
       "3, pinch, dispatcher, fullscreen"
       "4, pinchout, dispatcher, killactive"

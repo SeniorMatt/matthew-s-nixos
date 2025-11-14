8e21e9bbe0e05ddec2bc770473bf6931a114401d
Senior Matthew <matthew.prakhov@gmail.com>
2025-11-14 11:36:30 +0500

    2.5.1
    - Messed up with wallpapers slightly

--- etc/nixos/hosts/t480/configuration.nix
+++ etc/nixos/hosts/t480/configuration.nix
@@ -120,8 +120,8 @@ in {
       sudo cp -r /etc/nixos/ ~/Documents/GitHub/Matthew-s-NixOS/etc/
       sudo rm -f ~/Documents/GitHub/Matthew-s-NixOS/etc/nixos/hardware-configuration.nix
       echo "Copying Wallpapers..."
-      sudo cp -r ~/Pictures/wallpapers/ ~/Documents/GitHub/Matthew-s-NixOS/Pictures/
-      sudo cp -r ~/Pictures/wallpapers/catppuccin/ ~/Documents/GitHub/Matthew-s-NixOS/Pictures/wallpapers/
+      sudo cp -r ~/Pictures/wallpapers ~/Documents/GitHub/Matthew-s-NixOS/Pictures/
+      sudo cp -r ~/Pictures/wallpapers/catppuccin ~/Documents/GitHub/Matthew-s-NixOS/Pictures/wallpapers/
       sudo cp ~/Pictures/avatar.png ~/Documents/GitHub/Matthew-s-NixOS/Pictures/
       sudo cp ~/Pictures/avatar-circle.png ~/Documents/GitHub/Matthew-s-NixOS/Pictures/
     '')

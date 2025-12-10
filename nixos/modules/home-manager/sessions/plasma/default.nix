675e309b0ae6753992e40fe9587d7927981b8607
SeniorMatt <matthew.prakhov@gmail.com>
2025-12-10 10:25:03 +0500

    Updated Plasma's wallpaper

--- nixos/modules/home-manager/sessions/plasma/default.nix
+++ nixos/modules/home-manager/sessions/plasma/default.nix
@@ -1,7 +1,7 @@
 { inputs, pkgs, repoPath, ... }: 
 let
   wallpaperFolder = "${repoPath}/pictures/wallpapers";
-  wallpaper = "${wallpaperFolder}/schreckhorn_switzerland.jpeg";
+  wallpaper = "${wallpaperFolder}/kanistra-studio-20-workplace.jpg";
   generalFont = "Noto Sans";
   fixedFont = "JetBrainsMono";
   generalFontSize = 12;
